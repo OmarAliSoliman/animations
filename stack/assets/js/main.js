3c8a39620157c84ab2e6a6158caff505fd037c49
OmarAliSoliman <omarasmar9955@gmail.com>
2022-04-07 16:05:02 +0200

    stack

---
+++ stack/assets/js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+  new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity: 0.2,
+    image1: "./assets/images/01.png",
+    image2: "./assets/images/02.png",
+    displacementImage: "./assets/images/diss.png",
+    imagesRatio: 380 / 300,
+  });
+
+  gsap
+    .timeline()
+    .from(".links li", {
+      y: "100%",
+      opacity: 0,
+      ease: Expo.easeInOut,
+      stagger: 0.5,
+    });
+
+  gsap
+    .timeline()
+    .to(".overlay", {
+      top: "-100vh",
+      ease: Expo.easeInOut,
+      zIndex: "-1",
+      stagger: .2
+    })
+    .from(".content h1 .hidetext", {
+      y: "100%",
+      duration: 0.9,
+      opacity: 0,
+      ease: Expo.easeInOut,
+    })
+    .from(
+      ".content h3",
+      {
+        x: "-100%",
+        duration: 1.2,
+        opacity: 0,
+        ease: Expo.easeInOut,
+      },
+      "-=1"
+    )
+    .from(".content h2 .hidetext", {
+      y: "100%",
+      duration: 0.9,
+      opacity: 0,
+      ease: Expo.easeInOut,
+    }, "1")
+    .from(".content p .hidetext", {
+      y: "100%",
+      duration: 0.9,
+      opacity: 0,
+      ease: Expo.easeInOut,
+    });
+});
